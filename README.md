05b1ede96f12c7dba6e5b0281b60c04cce891869
Samuel-Enyaude <enyaudesamuel@hotmail.com>
2024-08-19 16:55:17 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Integrated-Project-Maji-Ndogo-Part-4
+
+Translating data into actionable insights! 
+Condensing complex queries for strategic decisions, and optimizing efficiency.
---
+++ Setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Integrated-Project-Maji-Ndogo-Part-4',
+    version='0.4',
+    license='MIT',
+    description='EDSA example SQL package',
+    install_requires=['SQL', 'Mysql', 'JupyterNotebook'],
+    url='https://github.com/Enyaude/Integrated-Project-Maji-Ndogo-Part-4.git',
+    author='<Enyaude>',
+    author_email='<enyaudesamuel@hotmail.com>'
+    )
